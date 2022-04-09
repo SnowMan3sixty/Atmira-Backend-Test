07d77aa961b4e7acb17ac22226176d9769af220c
Ruben Oncina <rubenonlo07@gmail.com>
2022-04-10 01:08:30 +0200

    /asteroids EP working correctly
    TODOS:
    - Handle exception if days parameter is not given
    - Handle exception if days parameter is not between 1 -7
    - Implement Junit tests

--- src/main/java/com/atmira/test/Backend_Java/model/Asteroid.java
+++ src/main/java/com/atmira/test/Backend_Java/model/Asteroid.java
@@ -1,40 +1,38 @@
 package com.atmira.test.Backend_Java.model;
 
-import java.util.Date;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor @Getter
 public class Asteroid {
 
-    private final String name;
-    
-    private final long diameterInKm;
-    private final long speedKmHour;
-    private final Date approachDate;
+    private final String name;    
+    private final Double diameterInKm;
+    private final Double speedKmHour;
+    private final String approachDate;
     private final String orbitingBody;
     
-    public Asteroid(String name, long diameterInKm, long speedKmHour, Date approachDate, String orbitingBody) {
+    public Asteroid(String name, Double diameterInKm, Double speedKmHour, String approachDate, String orbitingBody) {
         this.name = name;
         this.diameterInKm = diameterInKm;
         this.speedKmHour = speedKmHour;
         this.approachDate = approachDate;
         this.orbitingBody = orbitingBody;
-	}
+	}   
+
     public String getName() {
         return name;
     }
     
-    public long getDiameterInKm() {
+    public Double getDiameterInKm() {
         return diameterInKm;
     }
     
-    public long getSpeedKmHour() {
+    public Double getSpeedKmHour() {
         return speedKmHour;
     }
     
-    public Date getApproachDate() {
+    public String getApproachDate() {
         return approachDate;
     }
     
--- src/main/java/com/atmira/test/Backend_Java/service/AsteroidController.java
+++ src/main/java/com/atmira/test/Backend_Java/service/AsteroidController.java
@@ -1,10 +1,13 @@
 package com.atmira.test.Backend_Java.service;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import com.atmira.test.Backend_Java.model.Asteroid;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +17,74 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class AsteroidController {
 
+    private final String NASA_URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=%s&end_date=%s-12-12&api_key=%s";
+    private final String API_KEY = "DEMO_KEY";
+    //Use this other api key if the demo is not working
+    // private final String API_KEY = "zdUP8ElJv1cehFM0rsZVSQN7uBVxlDnu4diHlLSb";
+
+
     @GetMapping("/asteroids")
-    public Object[] potentiallyHazardousAsteroidsTop(@RequestParam(value="days") int days){
-        Asteroid[] topHazardAsteroids = new Asteroid[3];
-        topHazardAsteroids[0] = new Asteroid("asteroidName",1,2,new Date(), "planet");
-        topHazardAsteroids[1] =  new Asteroid("asteroidName2",11,22,new Date(), "planet2");
-        return topHazardAsteroids;
+    public List<Asteroid> potentiallyHazardousAsteroidsTop(@RequestParam(value="days") int days){        
+
+        JsonNode nasaApiResponse = callNasaApi(days);
+        List<Asteroid> hazardousAsteroidList = generateHazardousAsteroidList(nasaApiResponse);
+        hazardousAsteroidList = sortAsteroidsByDiamter(hazardousAsteroidList);     
+
+        if(hazardousAsteroidList.size() > 3 ){
+            hazardousAsteroidList = hazardousAsteroidList.subList(0,3);
+        }
+        return hazardousAsteroidList;
     } 
 
+    private JsonNode callNasaApi(int daysLapse){
+        RestTemplate restTemplate = new RestTemplate();
+        
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date currentDate = new Date();
+        String currentDateString = simpleDateFormat.format(currentDate);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+        c.add(Calendar.DATE, daysLapse);
+        String dateLapseString = simpleDateFormat.format(c.getTime());
+      
+        JsonNode apiResponse = restTemplate.getForObject(String.format(NASA_URL,currentDateString,dateLapseString,API_KEY),JsonNode.class);       
+        
+        return apiResponse;
+    }
+
+    private List<Asteroid> generateHazardousAsteroidList(JsonNode nasaApiResponse){
+        List<Asteroid> asteroidList = new ArrayList<Asteroid>();
+
+        for (JsonNode dateNode : nasaApiResponse.path("near_earth_objects")) {
+            for (JsonNode asteroidNode : dateNode) {
+                if(asteroidNode.get("is_potentially_hazardous_asteroid").asBoolean()){
+                    String name = asteroidNode.get("name").toString();
+                    Double diameterInKm = calculateAsteroidDiameter(asteroidNode.path("estimated_diameter").path("kilometers"));
+                    Double speedKmHour = asteroidNode.path("close_approach_data").get(0).path("relative_velocity").path("kilometers_per_hour").asDouble();
+                    String approachDate = asteroidNode.path("close_approach_data").get(0).path("close_approach_date").toString();
+                    String orbitingBody = asteroidNode.path("close_approach_data").get(0).path("orbiting_body").toString();
+                    asteroidList.add(new Asteroid(name, diameterInKm, speedKmHour, approachDate, orbitingBody));
+                }                              
+            }
+        }
+
+        return asteroidList;
+    }
    
+    private Double calculateAsteroidDiameter(JsonNode asteroidNode){
+        Double minDiameter = asteroidNode.path("estimated_diameter_min").asDouble();
+        Double maxDiameter = asteroidNode.path("estimated_diameter_max").asDouble();
+        
+        return (minDiameter + maxDiameter) / 2;
+    }
+
+    private List<Asteroid> sortAsteroidsByDiamter(List<Asteroid> asteroidList){  
+        asteroidList.sort((o1, o2)
+                      -> o2.getDiameterInKm().compareTo(o1.getDiameterInKm()));
+        return asteroidList;
+    }
 }
 
     
