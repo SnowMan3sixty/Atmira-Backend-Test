fa1d6e012b1dc7458e2ebd16570ba6f6e19488e1
Ruben Oncina <rubenonlo07@gmail.com>
2022-04-09 19:25:03 +0200

    project creation

---
+++ src/test/java/com/atmira/test/Backend_Java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmira.test.Backend_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
